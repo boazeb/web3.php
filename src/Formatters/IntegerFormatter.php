19d87d434dea4b6e42ce78dcce584950ffa6bedb
boazeb <boazeb@gmail.com>
2023-02-07 22:21:04 +0200

    fix https://github.com/kaiesh/web3.php/commit/bd6e5f10af0271dd15c105bd252dabaa201d5c85

--- src/Formatters/IntegerFormatter.php
+++ src/Formatters/IntegerFormatter.php
@@ -2,9 +2,9 @@
 
 /**
  * This file is part of web3.php package.
- * 
+ *
  * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
- * 
+ *
  * @author Peter Lai <alk03073135@gmail.com>
  * @license MIT
  */
@@ -19,7 +19,7 @@ class IntegerFormatter implements IFormatter
 {
     /**
      * format
-     * 
+     *
      * @param mixed $value
      * @return string
      */
@@ -33,12 +33,12 @@ class IntegerFormatter implements IFormatter
             $digit = intval($arguments[1]);
         }
         $bn = Utils::toBn($value);
-        $bnHex = $bn->toHex(true);
+        $bnHex = $bn->toHex(Utils::isNegative($value));
         $padded = mb_substr($bnHex, 0, 1);
 
         if ($padded !== 'f') {
             $padded = '0';
-        }        
+        }
         return implode('', array_fill(0, $digit-mb_strlen($bnHex), $padded)) . $bnHex;
     }
 }
